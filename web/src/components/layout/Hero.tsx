b4ec9eba0bb2eae1b181e693885f04790fc86b12
Lucas Albuquerque <lucastableless@gmail.com>
2025-04-20 18:39:45 +0400

    feat: make hero responsive

--- web/src/components/layout/Hero.tsx
+++ web/src/components/layout/Hero.tsx
@@ -2,18 +2,20 @@ import Button from "./Button";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center justify-center gap-8 mt-50">
-      <h2 className="text-center text-5xl font-semibold text-white">
-        Enterprise-Grade Document
-        <br /> Intelligence for Modern Businesses
-      </h2>
-      <p className="text-center text-gray-300">
-        Streamline your organization's document management with advanced search
-        capabilities <br />
-        and intelligent content analysis. Enhance productivity across your
-        enterprise with SierraDocs.
-      </p>
-      <Button href="/documents">Get Started</Button>
+    <div className="flex flex-col items-center justify-center gap-4 md:gap-8 h-[calc(100vh-120px)] px-4 md:px-0">
+      <div className="container mx-auto flex flex-col items-center justify-center">
+        <h2 className="text-center text-3xl md:text-5xl font-semibold text-white w-full max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-4xl">
+          Enterprise-Grade Document Intelligence for Modern Businesses
+        </h2>
+        <p className="text-center text-sm md:text-base text-gray-300 w-full max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-4xl mt-4 md:mt-8">
+          Streamline your organization's document management with advanced
+          search capabilities and intelligent content analysis. Enhance
+          productivity across your enterprise with SierraDocs.
+        </p>
+        <div className="mt-8">
+          <Button href="/documents">Get Started</Button>
+        </div>
+      </div>
     </div>
   );
 }
