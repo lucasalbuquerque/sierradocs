ab332ad3ee23caf5eb0b7885721292742d4024a3
Lucas Albuquerque <lucastableless@gmail.com>
2025-04-20 18:39:37 +0400

    feat: make header responsive

--- web/src/components/layout/Header.tsx
+++ web/src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center container mx-auto mt-10">
+    <div className="flex flex-col md:flex-row justify-between items-center container mx-auto mt-10 px-4 md:px-0 gap-4 md:gap-0">
       <Link href="/" className="flex items-center gap-2">
         <Image
           src="https://cdn.prod.website-files.com/662b316c8b82b058df121f76/66663ade08d68f71f7819e1c_sierra-logo.svg"
@@ -11,14 +11,14 @@ export default function Header() {
           width={50}
           height={50}
         />
-        <h1 className="text-white text-2xl font-bold">SierraDocs</h1>
+        <h1 className="text-white text-xl md:text-2xl font-bold">SierraDocs</h1>
       </Link>
       <nav className="flex items-center">
         <ul className="flex gap-4">
-          <li className="font-extralight text-white hover:text-gray-300 transition-colors duration-300">
+          <li className="font-extralight text-sm md:text-base text-white hover:text-gray-300 transition-colors duration-300">
             <Link href="/documents">Documents</Link>
           </li>
-          <li className="font-extralight text-white hover:text-gray-300 transition-colors duration-300">
+          <li className="font-extralight text-sm md:text-base text-white hover:text-gray-300 transition-colors duration-300">
             <Link href="/ai-assistant">AI Assistant</Link>
           </li>
         </ul>
